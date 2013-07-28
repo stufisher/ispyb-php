e4ad6df5cb80e3589b207c65d3c654a7627690c8
stufisher <stu@q3f.org>
2013-07-28 11:17:45 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+    require_once('config.php');
+    
+    require_once 'CAS/CAS.php';
+    phpCAS::client(CAS_VERSION_2_0, 'stageauth.diamond.ac.uk', 443, '/cas');
+    phpCAS::setNoCasServerValidation();
+    phpCAS::forceAuthentication();
+    
+    $parts = explode('/', $_SERVER['REQUEST_URI']);
+    array_shift($parts);
+
+    date_default_timezone_set('Europe/London');
+    
+    include_once('includes/class.page.php');
+    include_once('includes/class.db.php');
+    include_once('includes/class.template.php');
+    
+    $db = new Oracle($isb['user'], $isb['pass'], $isb['db']);
+    
+    $pages = array(
+                   'ajax' => array('Ajax', ''),
+                   'image' => array('Image', ''),
+                   'robot' => array('Robot', 'Robot Statistics'),
+                   'dc' => array('DC', 'Data Collections'),
+                   'fault' => array('Fault', 'Fault Logging'),
+                   'vstat' => array('Visit', 'Visit Statistics'),
+                   'log' => array('Log', 'Visit Summary'),
+                   );
+    
+    if (array_key_exists($parts[0], $pages)) {
+        $page = $parts[0];
+        array_shift($parts);
+    } else {
+        $page = 'log';
+    }
+    
+    $class = 'includes/pages/class.'.$page.'.php';
+    if (array_key_exists($page, $pages) && file_exists($class)) {
+        include_once($class);
+        $pg = new $pages[$page][0]($db, $parts);
+        
+    } else {
+        # 404 here
+        
+    }
+    
+?>
