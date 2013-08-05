745eb20d737274286ee87bbee634c9f46b3c6245
stufisher <stu@q3f.org>
2013-08-05 14:43:50 +0100

    Add prepared statements to oracle module

--- includes/class.db.php
+++ includes/class.db.php
@@ -67,6 +67,54 @@
         }
         
         
+        # Perform a database query
+        function pq($query, $args=array()) {
+            if ($this->debug) {
+                print '<h1 class="debug">Debug: Oracle</h1>';
+                print SqlFormatter::format($query);
+            }
+
+            $stid = oci_parse($this->conn, $query);
+            if (!$stid) {
+                $e = oci_error($conn);
+                trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+            }
+            
+            
+            // Get variables for prepared query
+            $arg_count = substr_count($query, ':');
+            if (sizeof($args) != $arg_count) trigger_error('Number of bound variables does not match number of arguments', E_USER_ERROR);
+            
+            for ($i = 0; $i < $arg_count; $i++) {
+                oci_bind_by_name($stid, ':'.($i+1), $args[$i]);
+            }
+            
+            
+            // Add a bound variable incase we need it
+            if (strpos($query, ':id') !== false) oci_bind_by_name($stid, ":id", $this->id, 8);
+            
+            // Perform the logic of the query
+            $r = oci_execute($stid);
+            if (!$r) {
+                $e = oci_error($stid);
+                trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+            }
+            
+            $data = array();
+            
+            if (strpos($query, 'INSERT') === false && strpos($query, 'UPDATE') === false) {
+                while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+                    #array_push($data, json_decode(json_encode($row), FALSE));
+                    array_push($data, $row);
+                }
+            }
+            
+            oci_free_statement($stid);
+            
+            return sizeof($data) == 0 ? array() : $data;
+        }        
+        
+        
         # Cleanup when finished
         function __destruct() {
             return;
