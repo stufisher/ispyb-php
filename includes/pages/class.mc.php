2957ae257ba9bec25ae84911a77874fedb1cbbd2
stufisher <stu@q3f.org>
2013-10-11 11:43:57 +0100

    Check for visit before showing mc page

--- includes/pages/class.mc.php
+++ includes/pages/class.mc.php
@@ -15,7 +15,11 @@
         function _data_collections() {
             if (!$this->has_arg('visit')) $this->error('No visit specified');
             
+            $info = $this->db->pq("SELECT case when sysdate between s.startdate and s.enddate then 1 else 0 end as active, s.sessionid, s.beamlinename as bl, vr.run, vr.runid, TO_CHAR(s.startdate, 'YYYY') as yr FROM ispyb4a_db.v_run vr INNER JOIN ispyb4a_db.blsession s ON (s.startdate BETWEEN vr.startdate AND vr.enddate) INNER JOIN ispyb4a_db.proposal p ON (p.proposalid = s.proposalid) WHERE  p.proposalcode || p.proposalnumber || '-' || s.visit_number LIKE :1", array($this->arg('visit')));
             
+            if (!sizeof($info)) {
+                $this->msg('No such visit', 'That visit doesnt appear to exist');
+            } else $info = $info[0];            
             
             $this->template('Multi-Crystal Integration', array($this->arg('visit')), array('/visit/'.$this->arg('visit')));
             
@@ -31,7 +35,11 @@
         function _blend() {
             if (!$this->has_arg('visit')) $this->error('No visit specified');
             
+            $info = $this->db->pq("SELECT case when sysdate between s.startdate and s.enddate then 1 else 0 end as active, s.sessionid, s.beamlinename as bl, vr.run, vr.runid, TO_CHAR(s.startdate, 'YYYY') as yr FROM ispyb4a_db.v_run vr INNER JOIN ispyb4a_db.blsession s ON (s.startdate BETWEEN vr.startdate AND vr.enddate) INNER JOIN ispyb4a_db.proposal p ON (p.proposalid = s.proposalid) WHERE  p.proposalcode || p.proposalnumber || '-' || s.visit_number LIKE :1", array($this->arg('visit')));
             
+            if (!sizeof($info)) {
+                $this->msg('No such visit', 'That visit doesnt appear to exist');
+            } else $info = $info[0];            
             
             $this->template('Multi-Crystal Integration - Blend', array($this->arg('visit'), 'Blend'), array('/visit/'.$this->arg('visit'), ''));
             
