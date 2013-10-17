fbf9c3f08cd9f91056f881a4de04cc806a09fc78
stufisher <stu@q3f.org>
2013-10-17 11:57:31 +0100

    Add current date to start page

--- templates/pages/log.php
+++ templates/pages/log.php
@@ -1,9 +1,12 @@
     <div class="data_collection">
+        <h1 class="center"><?php echo date('l j F Y') ?></h1>
+
         <h1>Last Visits</h1>
         <ul class="latest">
             <?php echo $visit_listl ?>
         </ul>
         <div class="clear"></div>
+
         <h1>Next Visits</h1>
         <ul class="latest">
         <?php echo $visit_listn ?>
