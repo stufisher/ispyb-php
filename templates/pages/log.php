f86f3a7133b7c150b2f298bc45295aaeeb54a413
stufisher <stu@q3f.org>
2013-12-02 10:37:13 +0000

    Minor style changes

--- templates/pages/log.php
+++ templates/pages/log.php
@@ -1,15 +1,15 @@
-    <div class="data_collection">
-        <h1 class="center"><?php echo date('l j F Y') ?></h1>
+    <h1 class="center nou">Today is <?php echo date('l j F Y') ?></h1>
 
-        <h1>Last Visits</h1>
-        <ul class="latest">
+    <h1>Last Visits</h1>
+    <div class="data_collection">
+        <ul class="latest clearfix">
             <?php echo $visit_listl ?>
         </ul>
-        <div class="clear"></div>
+    </div>
 
-        <h1>Next Visits</h1>
-        <ul class="latest">
+    <h1>Next Visits</h1>
+    <div class="data_collection">
+        <ul class="latest clearfix">
         <?php echo $visit_listn ?>
         </ul>
-        <div class="clear"></div>
     </div>
