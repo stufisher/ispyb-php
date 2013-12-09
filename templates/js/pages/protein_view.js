b04481594c8959b22ce9c1d8e84e48da40404243
stufisher <stu@q3f.org>
2013-12-09 16:50:40 +0000

    Missing text:false for button on view protein page

--- templates/js/pages/protein_view.js
+++ templates/js/pages/protein_view.js
@@ -24,7 +24,7 @@ $(function() {
   
   function _map_callbacks() {
     //setTimeout(function() {
-      $('a.view').button({ icons: { primary: 'ui-icon-search' } })
+      $('a.view').button({ icons: { primary: 'ui-icon-search' }, text: false })
     //}, 500)
   }
 
