cd27011e7b18eb61851187bb6e11d6d1e8d77cd0
stufisher <stu@q3f.org>
2013-12-11 14:38:26 +0000

    Make tracking page work

--- includes/pages/class.tracking.php
+++ includes/pages/class.tracking.php
@@ -2,7 +2,9 @@
 
     class Tracking extends Page {
         
-        var $arg_list = array('submit' => '\d');
+        var $arg_list = array('submit' => '\d',
+                              'dewar' => '([\w-])+',
+                              'location' => '([\w-])+');
         var $dispatch = array('main' => '_index');
         var $def = 'main';
         
@@ -15,11 +17,23 @@
         function _index() {
             $this->template('Dewar Tracking');
             $this->t->minimal();
+            $this->t->error = '';
+            $this->t->submit = $this->has_arg('submit');
             
-            if ($this->has_arg('submit')) {
-                $this->t->submit = 1;
-            } else {
-                $this->t->submit = 0;
+            if ($this->t->submit) {
+                if ($this->has_arg('dewar') && $this->has_arg('location')) {
+                    $dew = $this->db->pq("SELECT d.dewarid,s.shippingid FROM ispyb4a_db.dewar d INNER JOIN ispyb4a_db.shipping s ON s.shippingid = d.shippingid WHERE lower(barcode) LIKE lower(:1)", array($this->arg('dewar')));
+                    if (!sizeof($dew)) {
+                        $this->t->error = 'The specified dewar doesnt exist';
+                    
+                    } else {
+                        $dew = $dew[0];
+                        $this->db->pq("INSERT INTO ispyb4a_db.dewartransporthistory (dewartransporthistoryid,dewarid,dewarstatus,storagelocation,arrivaldate) VALUES (s_dewartransporthistory.nextval,:1,'at DLS',:2,CURRENT_TIMESTAMP)", array($dew['DEWARID'], $this->arg('location')));
+                        
+                        $this->db->pq("UPDATE ispyb4a_db.dewar set dewarstatus='at DLS', storagelocation=:2 WHERE dewarid=:1", array($dew['DEWARID'], $this->arg('location')));
+                        $this->db->pq("UPDATE ispyb4a_db.shipping set shippingstatus='at DLS' WHERE shippingid=:1", array($dew['SHIPPINGID']));
+                    }
+                }
 
             }
             
