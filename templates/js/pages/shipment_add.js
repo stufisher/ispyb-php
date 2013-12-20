d6831406346221f9b95f75ec307ecac98d78affa
stufisher <stu@q3f.org>
2013-12-20 18:00:15 +0000

    Lookup shipping details for lab contact

--- templates/js/pages/shipment_add.js
+++ templates/js/pages/shipment_add.js
@@ -14,4 +14,22 @@ $(function() {
         }
     }
   })
+  
+  
+  function _get_lc(lid) {
+    $.ajax({
+      url: '/shipment/ajax/lcd/lcid/'+lid,
+      type: 'GET',
+      dataType: 'json',
+      timeout: 5000,
+      success: function(j){
+        $('input[name=couriername]').val(j['DEFAULTCOURRIERCOMPANY'])
+        $('input[name=courierno]').val(j['COURIERACCOUNT'])
+      }       
+    })
+  }
+  
+  $('select[name=lcret]').change(function() { _get_lc($(this).val()) })
+  _get_lc($('select[name=lcret]').val())
+  
 })
