fc35a635a4f61052db5b865022adf4e835555cd4
stufisher <stu@q3f.org>
2014-01-02 09:21:12 +0000

    Add responsive behaviour to table, add search box

--- templates/js/pages/shipment.js
+++ templates/js/pages/shipment.js
@@ -1,18 +1,34 @@
 $(function() {
   var dt = {sPaginationType: 'full_numbers',
-            bAutoWidth:false ,
+            bAutoWidth: false,
             aaSorting: [[ 0, 'desc' ]],
             fnDrawCallback: _iconify,
   }
   
-  if ($(window).width() <= 600) dt = $.extend({
+  /*if ($(window).width() <= 600) dt = $.extend({
         'bScrollCollapse': true,
         'sScrollX': '100%',
-  }, dt)
+  }, dt)*/
   
-  $('.robot_actions').dataTable(dt)
+  var dt = $('.robot_actions').dataTable(dt)
+  $('.table input').focus()
+  
+  $('input.search-mobile').keyup(function() {
+    $('.dataTables_filter input').val($(this).val()).trigger('keyup')
+  }).parent('span').addClass('enable')
+  $('#sidebar,.cont_wrap').addClass('searchbox')
   
   $('a.add').button({ icons: { primary: 'ui-icon-plus' } })
+  
+  $(window).resize(function() { _resize() })
+  
+  function _resize() {
+    $.each([2,3,5,6],function(i,n) {
+      dt.fnSetColumnVis(n, !($(window).width() <= 600))
+    })
+  }
+  
+  _resize()
   
   function _iconify() {
     $('a.view').button({ icons: { primary: 'ui-icon-search' }, text: false })
