152ba86f1bb9a3b99b6ae61edee51a720c08f2f0
stufisher <stu@q3f.org>
2014-01-02 09:11:12 +0000

    Add mobile breakpoints

--- templates/pages/cell.php
+++ templates/pages/cell.php
@@ -4,23 +4,31 @@
     <p class="help">Results are ordered by &quot;Distance&quot; from the search unit cell parameters</p>
 
     <div class="cell_param data_collection">
-        <label>PDB Code: <input type="text" name="pdb" /></label>
+        <span class="pdb">
+            <label>PDB Code: <input type="text" name="pdb" /></label>
+            <button name="get_pdb" title="Retrieve details for the selected PDB code">Get</button>
+        </span>
 
-        <button name="get_pdb" title="Retrieve details for the selected PDB code">Get</button>
+        <span class="cell">
+            <label>a <input type="text" name="a" value="" /></label>
+            <label>b <input type="text" name="b" value="" /></label>
+            <label>c <input type="text" name="c" value="" /></label>
+        </span>
 
-        <label>a <input type="text" name="a" value="" /></label>
-        <label>b <input type="text" name="b" value="" /></label>
-        <label>c <input type="text" name="c" value="" /></label>
-        <label>&alpha; <input type="text" name="al" value="" /></label>
-        <label>&beta; <input type="text" name="be" value="" /></label>
-        <label>&gamma; <input type="text" name="ga" value="" /></label>
+        <span class="cell">
+            <label>&alpha; <input type="text" name="al" value="" /></label>
+            <label>&beta; <input type="text" name="be" value="" /></label>
+            <label>&gamma; <input type="text" name="ga" value="" /></label>
+        </span>
 
-        <label>Res <input type="text" name="res" title="Resolution limit to match to data collection" /></label>
-        <!--<label>SG <input type="text" name="sg" /></label>-->
+        <span class="opts">
+            <label>Res <input type="text" name="res" title="Resolution limit to match to data collection" /></label>
+            <!--<label>SG <input type="text" name="sg" /></label>-->
 
-        <label>Tolerance: <input type="text" name="tol" value="0.01" title="Tolerance for cell parameter search, default 1%" /></label>
+            <label>Tolerance: <input type="text" name="tol" value="0.01" title="Tolerance for cell parameter search, default 1%" /></label>
 
-        <button name="lookup" title="Search for data collections corresponding to the unit cell supplied">Look Up</button>
+            <button name="lookup" title="Search for data collections corresponding to the unit cell supplied">Look Up</button>
+        </span>
     </div>
 
     <div class="pdb_details_not_found data_collection">
@@ -42,6 +50,8 @@
     <div class="search">
         <span class="count"></span> Data Collections Found
     </div>
+
+    <h1>Search Results</h1>
 
 
     <div class="data_collections">
