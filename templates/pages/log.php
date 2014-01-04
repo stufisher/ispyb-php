8d97984739e1bfe61f206c2c8d7f01176afb4dd9
stufisher <stu@q3f.org>
2014-01-04 16:41:20 +0000

    Use existing list styling

--- templates/pages/log.php
+++ templates/pages/log.php
@@ -1,15 +1,11 @@
     <h1 class="center nou">Today is <?php echo date('l j F Y') ?></h1>
 
     <h1>Last Visits</h1>
-    <div class="data_collection">
-        <ul class="latest clearfix">
-            <?php echo $visit_listl ?>
-        </ul>
-    </div>
+    <ul class="ul full clearfix">
+        <?php echo $visit_listl ?>
+    </ul>
 
     <h1>Next Visits</h1>
-    <div class="data_collection">
-        <ul class="latest clearfix">
-        <?php echo $visit_listn ?>
-        </ul>
-    </div>
+    <ul class="ul full clearfix">
+    <?php echo $visit_listn ?>
+    </ul>
