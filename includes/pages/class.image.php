08f347693a2503ca3c56b1b04a28b43744609451
stufisher <stu@q3f.org>
2014-01-10 16:12:29 +0000

    debugging for di viewer

--- includes/pages/class.image.php
+++ includes/pages/class.image.php
@@ -107,8 +107,9 @@
                 if (!file_exists('/tmp/cbf2jpg.sh')) copy('./cbf2jpg.sh', '/tmp/cbf2jpg.sh');
                 
                 exec('/tmp/cbf2jpg.sh '.$info['LOC'].' '.$info['FT'].' '.$n.' '.$out);
-                $img = imagecreatefromjpeg($out);
-                imagejpeg($img,$out,80);
+                error_log('/tmp/cbf2jpg.sh '.$info['LOC'].' '.$info['FT'].' '.$n.' '.$out);
+                #$img = imagecreatefromjpeg($out);
+                #imagejpeg($img,$out,80);
             }
             
             if (file_exists($out)) {
