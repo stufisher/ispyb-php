960ec6ebe0571b0fc32058d6bea5136b65ea3c7d
stufisher <stu@q3f.org>
2014-01-29 14:38:38 +0000

    use timeout threads to stop scrolls fighting

--- templates/js/pages/dc.js
+++ templates/js/pages/dc.js
@@ -2,32 +2,46 @@ $(function() {
   var lp = 0
   var lp2 = 0
   
+  
+  var d1 = null
+  var d2 = null
   var last = 0
   var lastd = 0
+  
   var disablem = true
   var disabled = true
+  
+  var thread1 = null
+  var thread2 = null
   
   // Scroll details based on day
   $('.calendar_days').scroll(function(e) {
     if (disabled) return
-    disablem = true
 
     $('li',this).each(function(i,d) {
       var cdp = $(d).offset().left
-      if (Math.abs(cdp) < 5) {
-        var day = $(d).attr('day')
-        if (day != last) {
-          if (!$(d).hasClass('no_event')) {
-            var dp = $('.calendar_main li[day='+day+']').offset().top
-            $('.calendar_main').animate({scrollTop: dp-$('.calendar_main').offset().top+$('.calendar_main').scrollTop() })
-          }
+      if (Math.abs(cdp) < 15) {
+        if (!$(d).hasClass('no_event')) {
+          d1 = $(d).attr('day')
         }
-        last = day;
       }
     })
-    setTimeout(function() {
-      disablem = false
-    }, 1000)
+                             
+    clearTimeout(thread1)
+    thread1 = setTimeout(function() {
+        console.log('fired cal days scroll')
+        disablem = true
+
+        if (d1 != last) {
+          var dp = $('.calendar_main li[day='+d1+']').offset().top
+          $('.calendar_main').animate({scrollTop: dp-$('.calendar_main').offset().top+$('.calendar_main').scrollTop() })
+        }
+        last = d1;
+         
+        setTimeout(function() {
+          disablem = false
+        }, 1000)
+    },100)
   })
   
   
@@ -44,23 +58,28 @@ $(function() {
   // Scroll day based on details
   $('.calendar_main').scroll(function(e) {
     if (disablem) return
-    disabled = true
-                             
+             
     $('li[day]:not(.no_event)',this).each(function(i,d) {
       var cdp = $(d).offset().top - $('.calendar_main').offset().top
       if (Math.abs(cdp) < 5) {
-        var day = $(d).attr('day')
-        if (day != lastd) {
-          var dp = $('.calendar_days li[day='+day+']').offset().left
-          $('.calendar_days').animate({scrollLeft: dp-$('.calendar_days').offset().left+$('.calendar_days').scrollLeft() })
-        }
-        lastd = day;
+        d2 = $(d).attr('day')
       }
     })
                              
-    setTimeout(function() {
-      disabled = false
-    },1000)
+    clearTimeout(thread2)
+    thread2 = setTimeout(function() {
+      disabled = true
+                             
+      if (d2 != lastd) {
+        var dp = $('.calendar_days li[day='+d2+']').offset().left
+        $('.calendar_days').animate({scrollLeft: dp-$('.calendar_days').offset().left+$('.calendar_days').scrollLeft() })
+      }
+      lastd = d2;
+                             
+      setTimeout(function() {
+        disabled = false
+      },1000)
+    },100)
   })
   
   
