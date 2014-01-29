65d56db1f2348403c6439d7a36589c6420060683
stufisher <stu@q3f.org>
2014-01-29 16:17:16 +0000

    bind the right variable

--- OracleSession.php
+++ OracleSession.php
@@ -63,7 +63,7 @@
 
         public function _gc($max){
             $old = date('d-m-Y H:i', time() - $max);
-            $this->db->pq("DELETE FROM genericdata WHERE parametervaluedate < TO_DATE(:old, 'DD-MM-YYYY HH24:MI')", array($old));
+            $this->db->pq("DELETE FROM genericdata WHERE parametervaluedate < TO_DATE(:1, 'DD-MM-YYYY HH24:MI')", array($old));
 
             return true;
         }
