43a4300e584b4327a2844aee3962e6029765e846
stufisher <stu@q3f.org>
2014-01-31 13:17:00 +0000

    Add puck to container registration

--- templates/pages/container_add.php
+++ templates/pages/container_add.php
@@ -10,15 +10,20 @@
 
     <div class="confirm"></div>
 
-    <h1>Add Container</h1>
+    <a name="top"></a>
+    <h1 class="no_mobile">Add Container</h1>
 
     <p class="help">This page allows you to add containers to the selected dewar and shipment. If the protein you want to use isnt listed type in a new name and press tab. This will create a new protein</p>
 
-    <button class="pf" title="Paste container contents from clipboard"><i class="fa fa-paste"></i> Paste from Spreadsheet</button>
-
     <form method="post" id="add_container">
 
-    <div class="form">
+    <div class="form clearfix">
+        <button class="pf no_mobile" title="Paste container contents from clipboard"><i class="fa fa-paste"></i> Paste from Spreadsheet</button>
+
+        <div class="puck" title="Click to jump to a position in the puck">
+            <canvas></canvas>
+        </div>
+
         <ul>
             <li>
                 <span class="label">Shipment</span>
@@ -57,10 +62,15 @@
                     <td><?php echo ($i+1)?></td>
                     <td><select class="protein" name="p[]"></select>
                     <td><input class="sname" type="text" name="n[]" /></td>
-                    <td><select class="sg" name="sg[]"><?php echo $sgs ?></select>
+                    <td><select class="sg" name="sg[]"><?php echo $sgs ?></select></td>
                     <td><input class="code" type="text" name="b[]" /></td>
                     <td><input class="comment" type="text" name="c[]" /></td>
-                    <td><button class="clone" title="Clone this sample">Clone Sample</button> <!--<button class="insert" title="Insert Row">Insert Row</button>--> <button class="delete" title="Remove this sample">Delete Sample</button>
+                    <td>
+                        <button class="clone" title="Clone this sample">Clone Sample</button>
+                        <!--<button class="insert" title="Insert Row">Insert Row</button>-->
+                        <button class="delete" title="Remove this sample">Delete Sample</button>
+                        <span class="top r"><a href="#top">Top</a>
+                    </td>
                 </tr>
                 <?php endfor; ?>
             </tbody>
