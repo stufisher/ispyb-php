5be216ddbff661eb7f460bd8676ab057f1ff6560
stufisher <stu@q3f.org>
2014-02-02 17:41:35 +0000

    Welcome message

--- templates/pages/log.php
+++ templates/pages/log.php
@@ -1,5 +1,8 @@
-    <h1 class="center nou">Today is <?php echo date('l j F Y') ?></h1>
+    <p class="help">
+    This is the new interface to ISPyB at Diamond. If you want to escape to the old version click <a href="https://ispyb.diamond.ac.uk">here</a>
+    </p>
 
+    <h1 class="center nou">Today is <?php echo date('l j F Y') ?></h1>
 
     <h1>Next Visits</h1>
     <ul class="ul hover full clearfix">
