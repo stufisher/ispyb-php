9baf8fd7d3b4147e12da95f96214363425f7f3f6
stufisher <stu@q3f.org>
2014-03-08 11:45:28 +0000

    Remove ispyb link from log page

--- templates/pages/log.php
+++ templates/pages/log.php
@@ -1,6 +1,3 @@
-    <p class="help">
-    This is the new interface to ISPyB at Diamond. If you want to escape to the old version click <a href="https://ispyb.diamond.ac.uk">here</a>
-    </p>
 
     <h1 class="center nou">Today is <?php echo date('l j F Y') ?></h1>
 
