d7685d0909394df08acc4200178a05c1b659097f
stufisher <stu@q3f.org>
2014-03-24 14:22:59 +0000

    Initial ability to order auto processing based on metrics

--- templates/js/pages/dc_summary.js
+++ templates/js/pages/dc_summary.js
@@ -62,14 +62,14 @@ $(function() {
                 '<td></td>'+
                 '<td></td>'+
                 '<td></td>'+
-                '<td><a href="/dc/visit/'+(is_visit ? visit : (prop+'-'+e['VN']))+'/id/'+e['ID']+'" class="view" title="View full details for the selected data collection">View Data Collection</a></td>'+
+                '<td><a href="/dc/visit/'+(is_visit ? visit : (prop+'-'+e['VN']))+'/id/'+e['ID']+'" class="view" title="View full details for the selected data collection">View Data Collection</a> <span></span></td>'+
             +'</tr>').appendTo('table.summary tbody')
         })
            
         $('a.view').button({ icons: { primary: 'ui-icon-search' }, text: false })
         _get_status();
            
-        if (!json[1].length) $('<tr><td colspan="10">No data collections found</td></tr>').appendTo('table.summary tbody')
+        if (!json[1].length) $('<tr><td colspan="13">No data collections found</td></tr>').appendTo('table.summary tbody')
       }
            
     })
@@ -126,7 +126,7 @@ $(function() {
             if (d.length) {
               if (dc['SID'] && !$(d).find('.sample').length) {
                 $(d).children('td').eq(1).html('<a href="/sample/sid/'+dc['SID']+'/visit/'+prop+'">' + dc['SAN'] + '</a> (m' + dc['SCON'] + 'p' + dc['SPOS']+')')
-              
+                $(d).attr('sg', dc['SG'] ? dc['SG'].replace(/\s+/g,'') : '')
               }
               $(d).attr('sample', true)
             }
@@ -139,7 +139,9 @@ $(function() {
   
   }
   
-  
+  $('button[name="update"]').button({ icons: { primary: 'ui-icon-refresh' } }).click(function() {
+    _get_details()
+  })
   
   function _get_details() {
     $('tr[type=data][proc=1]').each(function(i,r) {
@@ -153,12 +155,7 @@ $(function() {
           var data = json[1]
             
           if (size > 0) {
-             
-            var ord = {'Fast DP': 1, 'XIA2 3da': 3, 'XIA2 2da': 2, 'XIA2 3daii': 4};
-            var od = {}
-            $.each(data, function(id,e) { od[ord[e['TYPE']]] = id })
-             
-            var best = data[od[Math.max.apply(null,Object.keys(od))]]
+            var best = _score(data, $(r).attr('sg'))
             if (best) {
               var c = best['CELL']
               var s = best['SHELLS']
@@ -172,13 +169,13 @@ $(function() {
               })
              
               var st = 7
-              $(r).children('td').eq(st).html(best['SG'])
+              $(r).children('td').eq(st).html(best['SG']+'</br />'+best['TYPE'])
               $(r).children('td').eq(st+1).html(c['CELL_A']+' ('+c['CELL_AL']+')<br />'+c['CELL_B']+' ('+c['CELL_BE']+')<br />'+c['CELL_C']+' ('+c['CELL_GA']+')')
               $(r).children('td').eq(st+2).html(s['overall']['RLOW']+' - '+s['overall']['RHIGH']+'<br />'+s['innerShell']['RLOW']+' - '+s['innerShell']['RHIGH']+'<br />'+s['outerShell']['RLOW']+' - '+s['outerShell']['RHIGH'])
               $(r).children('td').eq(st+3).html(sc[0]+s['overall'][rm]+'</span><br />'+sc[1]+s['innerShell'][rm]+'</span><br />'+sc[2]+s['outerShell'][rm]+'</span>')
               $(r).children('td').eq(st+4).html(sp[0]+s['overall']['COMPLETENESS']+'</span><br />'+sp[1]+s['innerShell']['COMPLETENESS']+'</span><br />'+sp[2]+s['outerShell']['COMPLETENESS']+'</span>')
 
-              $(r).children('td').eq(st+5).append(' <a href="/download/id/'+$(r).attr('dcid')+'/aid/'+best['AID']+'" class="dll" title="Download MTZ file">Download MTZ file</a>')
+              $(r).children('td').eq(st+5).children('span').html(' <a href="/download/id/'+$(r).attr('dcid')+'/aid/'+best['AID']+'" class="dll" title="Download MTZ file">Download MTZ file</a>')
             }
           }
              
@@ -186,6 +183,62 @@ $(function() {
         }
       })
     })
+  }
+  
+  // Return 'best' autoprocessing
+  function _score(data, sg) {
+    // metric weights
+    var weights = { rmerge: $('input[name="rmerge"]').val(), res: $('input[name="res"]').val(), isigi: $('input[name="isigi"]').val(), c: $('input[name="comp"]').val(), sg: $('input[name="sg"]').is(':checked') }
+  
+    // shell weights - should probably have per shell metric weights...
+    var sweights = { overall: 1, innerShell: 0.5, outerShell: 1 }
+  
+    var max = {res: {}, isig: {}, resm: {}, rdiff: {}}
+    $.each(sweights, function(sh, w) {
+      max.res[sh] = 0
+      max.isig[sh] = 0
+      max.resm[sh] = 999
+      max.rdiff[sh] = 0
+    })
+  
+    $.each(data, function(i,d) {
+      $.each(sweights, function(sh, w) {
+        var s = d['SHELLS'][sh]
+        if (parseFloat(s['RHIGH']) > max.res[sh]) max.res[sh] = parseFloat(s['RHIGH']);
+        if (parseFloat(s['RHIGH']) < max.resm[sh]) max.resm[sh] = parseFloat(s['RHIGH']);
+        if (parseFloat(s['ISIGI']) > max.isig[sh]) max.isig[sh] = parseFloat(s['ISIGI']);
+      })
+    })
+  
+    $.each(sweights, function(sh, w) {
+      max.rdiff[sh] = max.res[sh] - max.resm[sh]
+    })
+  
+    var best = null
+    var best_score = 0
+    $.each(data, function(i,d) {
+      var score = 0
+      $.each(sweights, function(sh, w) {
+        var s = d['SHELLS'][sh]
+   
+        var res = ((max.res[sh] - parseFloat(s['RHIGH']))/max.rdiff[sh])*weights.res
+        var rmerge = (1 - parseFloat(s['RMERGE']))*weights.rmerge
+        var isigi = (parseFloat(s['ISIGI'])/max.isig[sh])*weights.isigi
+        var comp = (parseFloat(s['COMPLETENESS'])/100)*weights.c
+  
+        score += (res + rmerge + isigi + comp) * w
+      })
+           
+      score *= (weights.sg ? (sg ? sg == d['SG'].replace(/\s+/g,'') : 1) : 1)
+           
+      if (score > best_score) {
+        best_score = score
+        best = i
+      }
+           
+    })
+
+    return data[best]
   }
   
 });
--- templates/pages/dc_summary.php
+++ templates/pages/dc_summary.php
@@ -1,5 +1,14 @@
     <h1>Data Collection Summary for <?php echo $is_visit ? $visit : $prop ?></h1>
 
+    <div class="options data_collection">
+        <label>Resolution: <input type="text" name="res" value="1" /></label>
+        <label>Rmerge: <input type="text" name="rmerge" value="0.6" /></label>
+        <label>Completeness: <input type="text" name="comp" value="1" /></label>
+        <label>I/&sigma;(I): <input type="text" name="isigi" value="0.5" /></label>
+        <label>Match Spacegroup: <input type="checkbox" name="sg" value="1" /></label>
+        <button name="update">Update</button>
+    </div>
+
     <div class="search hide" title="Search the current data collections">
         <input type="text" name="search" placeholder="&#xf002;" />
     </div>
