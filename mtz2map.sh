d90334129e15c5d93e0045dceaebcd46ee79983d
stufisher <stu@q3f.org>
2014-04-15 14:51:09 +0100

    More changes to map generation, masking, peak finding

--- mtz2map.sh
+++ mtz2map.sh
@@ -6,12 +6,10 @@ export CCP4_MASTER=/dls_sw/apps/ccp4/x86_64/6.4.0/11oct2013/
 export CINCL=$CCP4_MASTER/ccp4-6.4.0/include
 export CLIBD=$CCP4_MASTER/ccp4-6.4.0/lib/data
 export CCP4_SCR=/tmp
-export fft=$CCP4_MASTER/ccp4-6.4.0/bin/fft
-export mm=$CCP4_MASTER/ccp4-6.4.0/bin/mapmask
-export pm=$CCP4_MASTER/ccp4-6.4.0/bin/peakmax
+export root=$CCP4_MASTER/ccp4-6.4.0/bin
 
 if [ $3 == 'dimple' ]; then
-$fft HKLIN $1 MAPOUT "/tmp/$2_dimple_2fofc.map.tmp" << eof
+$root/fft HKLIN $1 MAPOUT "/tmp/$2_dimple_2fofc.map.tmp" << eof
 title $2 2fofc
 xyzlim asu
 scale F1 1.0
@@ -20,11 +18,11 @@ F1=F SIG1=SIGF PHI=PH2FOFCWT W=FOM
 end
 eof
 
-$mm MAPIN "/tmp/$2_dimple_2fofc.map.tmp" MAPOUT "/tmp/$2_dimple_2fofc.map" XYZIN "$4" << eof
+$root/mapmask MAPIN "/tmp/$2_dimple_2fofc.map.tmp" MAPOUT "/tmp/$2_dimple_2fofc.map" XYZIN "$4" << eof
 BORDER 5
 eof
 
-$fft HKLIN $1 MAPOUT "/tmp/$2_dimple_fofc.map.tmp" << eof
+$root/fft HKLIN $1 MAPOUT "/tmp/$2_dimple_fofc.map.tmp" << eof
 title $2 fofc
 xyzlim asu
 scale F1 1.0
@@ -33,7 +31,7 @@ F1=F SIG1=SIGF PHI=PHFOFCWT W=FOM
 end
 eof
 
-$pm MAPIN "/tmp/$2_dimple_fofc.map.tmp" XYZOUT "/tmp/$2_dimple.peaks" XYZFRC "/tmp/$2_dimple_peaks.ha" << eof
+$root/peakmax MAPIN "/tmp/$2_dimple_fofc.map.tmp" XYZOUT "/tmp/$2_dimple.peaks.tmp" << eof
 threshhold -
 rms -
 3.0
@@ -44,7 +42,14 @@ atname OW
 chain X
 eof
 
-$mm MAPIN "/tmp/$2_dimple_fofc.map.tmp" MAPOUT "/tmp/$2_dimple_fofc.map" XYZIN "$4" << eof
+export SG=`awk '/^CRYST/{print substr($0,56,12)}' $4 | head -1 | sed 's/ //g'`
+$root/watpeak XYZIN "$4" PEAKS "/tmp/$2_dimple.peaks.tmp" XYZOUT "/tmp/$2_dimple.peaks" << eof
+distance 5.0
+symm $SG
+eof
+
+
+$root/mapmask MAPIN "/tmp/$2_dimple_fofc.map.tmp" MAPOUT "/tmp/$2_dimple_fofc.map" XYZIN "$4" << eof
 BORDER 5
 eof
 
@@ -53,7 +58,7 @@ gzip "/tmp/$2_dimple_fofc.map"
 
 
 else
-$fft HKLIN $1 MAPOUT "/tmp/$2_ep.map" << eof
+$root/fft HKLIN $1 MAPOUT "/tmp/$2_ep.map" << eof
 title $2 fofc
 xyzlim asu
 scale F1 1.0
