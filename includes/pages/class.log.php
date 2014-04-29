c4523d32982eb0abfe21f15536442e9b010c6a3e
stufisher <stu@q3f.org>
2014-04-29 10:53:44 +0100

    Maybe fix front page?

--- includes/pages/class.log.php
+++ includes/pages/class.log.php
@@ -23,21 +23,18 @@
             
             if ($this->staff) {
                 foreach(array('i02', 'i03', 'i04', 'i04-1', 'i24') as $b) {
-                    $visit = $this->db->pq('SELECT * FROM (SELECT case when sysdate between s.startdate and s.enddate then 1 else 0 end as active, p.proposalcode || p.proposalnumber || \'-\' || s.visit_number as vis, TO_CHAR(s.startdate, \'DD-MM-YYYY HH24:MI\') as st, TO_CHAR(s.enddate, \'DD-MM-YYYY HH24:MI\') as en,s.beamlinename as bl, s.sessionid FROM ispyb4a_db.blsession s INNER JOIN ispyb4a_db.proposal p ON (p.proposalid = s.proposalid) WHERE s.enddate <= SYSDATE AND s.beamlinename LIKE :1 ORDER BY s.enddate DESC) where rownum < 3', array($b));
+                    $visit = $this->db->pq('SELECT * FROM (SELECT case when sysdate between s.startdate and s.enddate then 1 else 0 end as active, p.proposalcode || p.proposalnumber || \'-\' || s.visit_number as vis, TO_CHAR(s.startdate, \'DD-MM-YYYY HH24:MI\') as st, TO_CHAR(s.enddate, \'DD-MM-YYYY HH24:MI\') as en,s.beamlinename as bl, s.sessionid FROM ispyb4a_db.blsession s INNER JOIN ispyb4a_db.proposal p ON (p.proposalid = s.proposalid) WHERE s.enddate <= SYSDATE AND s.beamlinename LIKE :1 ORDER BY s.enddate DESC) where rownum < 2', array($b));
 
-                    $visitn = $this->db->pq('SELECT * FROM (SELECT case when sysdate between s.startdate and s.enddate then 1 else 0 end as active, p.proposalcode || p.proposalnumber || \'-\' || s.visit_number as vis, TO_CHAR(s.startdate, \'DD-MM-YYYY HH24:MI\') as st, TO_CHAR(s.enddate, \'DD-MM-YYYY HH24:MI\') as en,s.beamlinename as bl, s.sessionid FROM ispyb4a_db.blsession s INNER JOIN ispyb4a_db.proposal p ON (p.proposalid = s.proposalid) WHERE s.startdate >= SYSDATE AND s.beamlinename LIKE:1 ORDER BY s.startdate) where rownum < 3', array($b));
+                    $visitn = $this->db->pq('SELECT * FROM (SELECT case when sysdate between s.startdate and s.enddate then 1 else 0 end as active, p.proposalcode || p.proposalnumber || \'-\' || s.visit_number as vis, TO_CHAR(s.startdate, \'DD-MM-YYYY HH24:MI\') as st, TO_CHAR(s.enddate, \'DD-MM-YYYY HH24:MI\') as en,s.beamlinename as bl, s.sessionid FROM ispyb4a_db.blsession s INNER JOIN ispyb4a_db.proposal p ON (p.proposalid = s.proposalid) WHERE s.enddate >= SYSDATE AND s.beamlinename LIKE:1 AND TO_CHAR(s.startdate,\'YYYY\') > 2009 ORDER BY s.startdate) where rownum < 2', array($b));
                     
                     if (sizeof($visit) > 0) {
-                        // Nasty hack to check for overwritten visits
-                        if ($visit[0]['ST'] == $visit[1]['ST']) $v = $visit[1];
-                        else $v = $visit[0];
+                        $v = $visit[0];
                         
                         array_push($visit_listl, '<li'.($v['ACTIVE'] ? ' class="active"' : '').'><h1>'.$b.$this->lc($v['SESSIONID']).'</h1><h2><a href="/dc/visit/'.$v['VIS'].'">'.$v['VIS'].'</a></h2><ul><li>Start: '.$v['ST'].'</li><li>End: '.$v['EN'].'</li><li><a href="/vstat/visit/'.$v['VIS'].'">Visit Statistics</a></li></ul></li>');
                     }
                     
                     if (sizeof($visitn) > 0) {
-                        if ($visitn[0]['ST'] == $visitn[1]['ST']) $v = $visitn[1];
-                        else $v = $visitn[0];
+                        $v = $visitn[0];
                         
                         array_push($visit_listn, '<li'.($v['ACTIVE'] ? ' class="active"' : '').'><h1>'.$b.$this->lc($v['SESSIONID']).'</h1><h2><a href="/dc/visit/'.$v['VIS'].'">'.$v['VIS'].'</a></h2><ul><li>Start: '.$v['ST'].'</li><li>End: '.$v['EN'].'</li><li><a href="/vstat/visit/'.$v['VIS'].'">Visit Statistics</a></li></ul></li>');
                     }
@@ -58,7 +55,7 @@
                     INNER JOIN investigation@DICAT_RO i ON (lower(i.visit_id) = p.proposalcode || p.proposalnumber || \'-\' || s.visit_number)
                     INNER JOIN investigationuser@DICAT_RO iu on i.id = iu.investigation_id
                     INNER JOIN user_@DICAT_RO u on u.id = iu.user_id
-                    WHERE u.name=:1 AND s.startdate >= SYSDATE ORDER BY s.enddate) where rownum < 6', array(phpCAS::getUser()));
+                    WHERE u.name=:1 AND s.enddate >= SYSDATE ORDER BY s.enddate) where rownum < 6', array(phpCAS::getUser()));
                                                             
                 
                 if (sizeof($visit) > 0) {
