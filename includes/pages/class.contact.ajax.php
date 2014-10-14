b16cb9b5daf96393c6bce6a5dccf6528a6be7c24
stufisher <stu@q3f.org>
2014-10-14 18:50:20 +0100

    various updates for lab contact ajax scripts.

--- includes/pages/class.contact.ajax.php
+++ includes/pages/class.contact.ajax.php
@@ -4,12 +4,26 @@
         
         var $arg_list = array('value' => '.*',
                               'cid' => '\d+',
+                              'cardname' => '([\w\s-])+',
+                              'familyname' => '([\w-])+',
+                              'givenname' => '([\w-])+',
+                              'phone' => '.*',
+                              'email' => '.*',
+                              'labname' => '([\w\s-])+',
+                              'address' => '([\w\s-\n])+',
+                              'courier' => '([\w\s-])+',
+                              'courieraccount' => '([\w-])+',
+                              'billingreference' => '([\w\s-])+',
+                              'transportvalue' => '\d+',
+                              'customsvalue' => '\d+',
                               'ty' => '\w+',
                               'iDisplayStart' => '\d+',
                               'iDisplayLength' => '\d+',
+                              'array' => '\d',
                               );
         var $dispatch = array('contacts' => '_get_contacts',
                               'update' => '_update_contact',
+                              'add' => '_add_contact',
                               );
         
         var $def = 'contacts';
@@ -30,6 +44,11 @@
             $args = array($p);
             $where = 'WHERE c.proposalid = :1';
             
+            if ($this->has_arg('cid')) {
+                $where .= ' AND c.labcontactid=:'.(sizeof($args)+1);
+                array_push($args, $this->arg('cid'));
+            }
+            
             $sta = $this->has_arg('iDisplayStart') ? $this->arg('iDisplayStart') : 0;
             $len = $this->has_arg('iDisplayLength') ? $this->arg('iDisplayLength') : 20;
             
@@ -49,7 +68,7 @@
             }
         
             $rows = $this->db->pq("SELECT outer.* FROM (SELECT ROWNUM rn, inner.* FROM (
-                                 SELECT c.labcontactid, c.cardname, pe.givenname, pe.familyname, pe.phonenumber, l.name as labname, l.address, l.city, l.country FROM ispyb4a_db.labcontact c INNER JOIN ispyb4a_db.person pe ON c.personid = pe.personid INNER JOIN ispyb4a_db.laboratory l ON l.laboratoryid = pe.laboratoryid INNER JOIN ispyb4a_db.proposal p ON p.proposalid = c.proposalid $where ORDER BY $order
+                                 SELECT c.labcontactid, c.cardname, pe.givenname, pe.familyname, pe.phonenumber, l.name as labname, l.address, l.city, l.country, c.courieraccount,  c.billingreference, c.defaultcourriercompany, c.dewaravgcustomsvalue, c.dewaravgtransportvalue, pe.emailaddress FROM ispyb4a_db.labcontact c INNER JOIN ispyb4a_db.person pe ON c.personid = pe.personid INNER JOIN ispyb4a_db.laboratory l ON l.laboratoryid = pe.laboratoryid INNER JOIN ispyb4a_db.proposal p ON p.proposalid = c.proposalid $where ORDER BY $order
                                   ) inner) outer WHERE outer.rn > :$st AND outer.rn <= :".($st+1), $args);
             
             $data = array();
@@ -61,9 +80,13 @@
                 array_push($data, array($r['CARDNAME'], $r['GIVENNAME'].' '.$r['FAMILYNAME'], str_replace("\n", '<br/>',  implode(', ', $addr)), $r['PHONENUMBER'], $r['LABNAME'], '<a class="view" title="View Lab Contact" href="/contact/cid/'.$r['LABCONTACTID'].'">View</a>'));
             }
             
-            $this->_output(array('iTotalRecords' => $tot,
+            if ($this->has_arg('cid')) {
+                if (sizeof($rows))$this->_output($rows[0]);
+                else $this->_error('No such contact');
+                
+            } else $this->_output(array('iTotalRecords' => $tot,
                                  'iTotalDisplayRecords' => $tot,
-                                 'aaData' => $data,
+                                 'aaData' => $this->has_arg('array') ? $rows : $data,
                            ));
         }
         
@@ -128,6 +151,44 @@
             
             print $v;
         }
+        
+        
+        
+        # ------------------------------------------------------------------------
+        # Add a new lab contact
+        function _add_contact() {
+            if (!$this->has_arg('prop')) $this->_error('No proposal selected');
+            
+            $valid = True;
+            foreach (array('cardname', 'familyname', 'givenname', 'labname', 'address') as $k) {
+                if (!$this->has_arg($k)) $valid = False;
+            }
+            
+            if (!$valid) $this->_error('Missing Fields');
+            
+            $this->db->pq("INSERT INTO ispyb4a_db.laboratory (laboratoryid,name,address) VALUES (s_laboratory.nextval, :1, :2) RETURNING laboratoryid INTO :id", array($this->arg('labname'), $this->arg('address')));
+            $lid = $this->db->id();
+            
+            $email = $this->has_arg('email') ? $this->arg('email') : '';
+            $phone = $this->has_arg('phone') ? $this->arg('phone') : '';
+            
+            $this->db->pq("INSERT INTO ispyb4a_db.person (personid, givenname, familyname, emailaddress, phonenumber, laboratoryid) VALUES (s_person.nextval, :1, :2, :3, :4, :5) RETURNING personid INTO :id", array($this->arg('givenname'), $this->arg('familyname'), $email, $phone, $lid));
+            
+            $pid = $this->db->id();
+            
+            $c = $this->has_arg('courier') ? $this->arg('courier') : '';
+            $ca = $this->has_arg('courieraccount') ? $this->arg('courieraccount') : '';
+            $br = $this->has_arg('billingreference') ? $this->arg('billingreference') : '';
+            $cv = $this->has_arg('customsvalue') ? $this->arg('customsvalue') : 0;
+            $tv = $this->has_arg('transportvalue') ? $this->arg('transportvalue') : 0;
+            
+            $this->db->pq("INSERT INTO ispyb4a_db.labcontact (labcontactid, cardname, defaultcourriercompany, courieraccount, billingreference, dewaravgcustomsvalue, dewaravgtransportvalue, proposalid, personid) VALUES (s_labcontact.nextval, :1, :2, :3, :4, :5, :6, :7, :8) RETURNING labcontactid INTO :id", array($this->arg('cardname'), $c, $ca, $br, $cv, $tv, $this->proposalid, $pid));
+            
+            $this->_output($this->db->id());
+            
+            
+        }
+
 
     }
 
