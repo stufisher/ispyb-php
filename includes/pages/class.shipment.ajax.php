afc39a69d5e08e002c12911fd645d24017bfea93
stufisher <stu@q3f.org>
2014-10-28 11:20:36 +0000

    Add ajax shipment registration, add filters for shipments

--- includes/pages/class.shipment.ajax.php
+++ includes/pages/class.shipment.ajax.php
@@ -19,6 +19,17 @@
                               'exp' => '\d+',
                               'container' => '([\w-])+',
                               
+                              'lcout' => '\d+',
+                              'lcret' => '\d+',
+                              'shippingname' => '([\w\s-])+',
+                              'shippingdate' => '\d+-\d+-\d+',
+                              'deliverydate' => '\d+-\d+-\d+',
+                              'couriername' => '\w+',
+                              'courierno' => '\w+',
+                              'safety' => '\w+',
+                              'dewars' => '\d+',
+                              'visit' => '\w+\d+-\d+',
+                              
                               'name' => '([\w-])+',
                               'comment' => '.*',
                               'spacegroup' => '\w+',
@@ -34,9 +45,11 @@
                               
                               'assigned' => '\d',
                               'bl' => '\w\d\d(-\d)?',
+                              'unassigned' => '\w\d\d(-\d)?',
                               );
         
-        var $dispatch = array('shipments' => '_get_shipments',
+        var $dispatch = array('add' => '_add_shipment',
+                              'shipments' => '_get_shipments',
                               'containers' => '_get_containers',
                               'containersall' => '_get_all_containers',
                               'samples' => '_get_samples',
@@ -87,7 +100,7 @@
                 array_push($args, $this->arg('sid'));
             }
             
-            $rows = $this->db->pq("SELECT s.deliveryagent_agentname, s.deliveryagent_agentcode, s.deliveryagent_shippingdate, s.deliveryagent_deliverydate, s.safetylevel, count(d.dewarid) as dcount,c.cardname as lcout, c2.cardname as lcret, s.shippingid, s.shippingname, s.shippingstatus,TO_CHAR(s.creationdate, 'DD-MM-YYYY') as created, s.isstorageshipping, s.shippingtype, s.comments FROM ispyb4a_db.proposal p INNER JOIN ispyb4a_db.shipping s ON p.proposalid = s.proposalid LEFT OUTER JOIN ispyb4a_db.labcontact c ON s.sendinglabcontactid = c.labcontactid LEFT OUTER JOIN ispyb4a_db.labcontact c2 ON s.returnlabcontactid = c2.labcontactid LEFT OUTER JOIN ispyb4a_db.dewar d ON d.shippingid = s.shippingid WHERE p.proposalcode || p.proposalnumber = :1 $where GROUP BY s.deliveryagent_agentname, s.deliveryagent_agentcode, s.deliveryagent_shippingdate, s.deliveryagent_deliverydate, s.safetylevel, c.cardname, c2.cardname, s.shippingid, s.shippingname, s.shippingstatus,TO_CHAR(s.creationdate, 'DD-MM-YYYY'), s.isstorageshipping, s.shippingtype, s.comments, s.creationdate ORDER BY s.creationdate DESC", $args);
+            $rows = $this->db->pq("SELECT s.deliveryagent_agentname, s.deliveryagent_agentcode, s.deliveryagent_shippingdate, s.deliveryagent_deliverydate, s.safetylevel, count(d.dewarid) as dcount,c.labcontactid as lcoutid, c.cardname as lcout, c2.cardname as lcret, c2.labcontactid as lcretid, s.shippingid, s.shippingname, s.shippingstatus,TO_CHAR(s.creationdate, 'DD-MM-YYYY') as created, s.isstorageshipping, s.shippingtype, s.comments FROM ispyb4a_db.proposal p INNER JOIN ispyb4a_db.shipping s ON p.proposalid = s.proposalid LEFT OUTER JOIN ispyb4a_db.labcontact c ON s.sendinglabcontactid = c.labcontactid LEFT OUTER JOIN ispyb4a_db.labcontact c2 ON s.returnlabcontactid = c2.labcontactid LEFT OUTER JOIN ispyb4a_db.dewar d ON d.shippingid = s.shippingid WHERE p.proposalcode || p.proposalnumber = :1 $where GROUP BY c.labcontactid, c2.labcontactid, s.deliveryagent_agentname, s.deliveryagent_agentcode, s.deliveryagent_shippingdate, s.deliveryagent_deliverydate, s.safetylevel, c.cardname, c2.cardname, s.shippingid, s.shippingname, s.shippingstatus,TO_CHAR(s.creationdate, 'DD-MM-YYYY'), s.isstorageshipping, s.shippingtype, s.comments, s.creationdate ORDER BY s.creationdate DESC", $args);
             
             if ($this->has_arg('sid')) {
                 if (sizeof($rows)) $this->_output($rows[0]);
@@ -467,12 +480,18 @@
             }
             
             if ($this->has_arg('assigned')) {
-                $where .= " AND d.dewarstatus LIKE 'processing'";
+                $where .= " AND d.dewarstatus LIKE 'processing' AND c.samplechangerlocation > 0";
             }
                 
             if ($this->has_arg('bl')) {
                 $where .= " AND c.beamlinelocation LIKE :".(sizeof($args)+1);
                 array_push($args, $this->arg('bl'));
+            }
+            
+            if ($this->has_arg('unassigned')) {
+                $where  .= " AND c.containerid NOT IN (SELECT c.containerid FROM ispyb4a_db.container c INNER JOIN ispyb4a_db.dewar d ON d.dewarid = c.dewarid WHERE d.dewarstatus LIKE 'processing' AND c.samplechangerlocation > 0 AND c.beamlinelocation=:".(sizeof($args)+1).")";
+                
+                array_push($args, $this->arg('unassigned'));
             }
                 
             
@@ -661,6 +680,53 @@
         }
         
         
+        # Ajax shipment registration
+        function _add_shipment() {
+            if (!$this->has_arg('prop')) $this->error('No proposal specified', 'Please select a proposal first');
+        
+            if (!$this->has_arg('shippingname')) $this->error('No shipment name specified', 'Please specify a shipment name');
+            
+            $sd = $this->has_arg('shippingdate') ? $this->arg('shippingdate') : '';
+            $dd = $this->has_arg('deliverydate') ? $this->arg('deliverydate') : '';
+            $com = $this->has_arg('comment') ? $this->arg('comment') : '';
+            
+            $this->db->pq("INSERT INTO ispyb4a_db.shipping (shippingid, proposalid, shippingname, deliveryagent_agentname, deliveryagent_agentcode, deliveryagent_shippingdate, deliveryagent_deliverydate, bltimestamp, creationdate, comments, sendinglabcontactid, returnlabcontactid, shippingstatus, safetylevel) VALUES (s_shipping.nextval,:1,:2,:3,:4,TO_DATE(:5,'DD-MM-YYYY'), TO_DATE(:6,'DD-MM-YYYY'),CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,:7,:8,:9,'opened',:10) RETURNING shippingid INTO :id", array($this->proposalid, $this->arg('shippingname'), $this->arg('couriername'), $this->arg('courierno'), $sd, $dd, $com, $this->arg('lcout'), $this->arg('lcret'), $this->arg('safety')));
+            
+            $sid = $this->db->id();
+            
+            $fc = array();
+            if (array_key_exists('fcodes', $_POST)) {
+                foreach ($_POST['fcodes'] as $i => $f) {
+                    if (preg_match('/^[\w-]+$/', $f)) {
+                        $fc[$i] = $f;
+                    } else $fc[$i] =  '';
+                }
+            }
+            
+            if ($this->has_arg('dewars')) {
+                if ($this->arg('dewars') > 0) {
+                    $exp = $this->has_arg('exp') ? $this->arg('exp') : '';
+                    
+                    for ($i = 0; $i < $this->arg('dewars'); $i++) {
+                        $n = $fc[$i] ? $fc[$i] : ('Dewar'.($i+1));
+                        
+                        $this->db->pq("INSERT INTO ispyb4a_db.dewar (dewarid,code,shippingid,bltimestamp,dewarstatus,firstexperimentid,facilitycode) VALUES (s_dewar.nextval,:1,:2,CURRENT_TIMESTAMP,'opened',:3,:4) RETURNING dewarid INTO :id", array($n, $sid, $exp, $fc[$i]));
+                        
+                        $id = $this->db->id();
+                        
+                        $vis = '';
+                        if ($exp) {
+                            $vr = $this->db->pq("SELECT s.beamlinename as bl,s.visit_number as vis FROM ispyb4a_db.blsession s WHERE s.sessionid=:1", array($exp));
+                            if (sizeof($vr)) $vis = '-'.$vr[0]['VIS'].'-'.$vr[0]['BL'];
+                        }
+                        
+                        $this->db->pq("UPDATE ispyb4a_db.dewar set barcode=:1 WHERE dewarid=:2", array($this->arg('prop').$vis.'-'.str_pad($id,7,'0',STR_PAD_LEFT), $id));
+                    }
+                }
+            }
+            
+            $this->_output($sid);
+        }
         
         
     }
