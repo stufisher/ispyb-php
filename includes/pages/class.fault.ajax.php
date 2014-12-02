ac26db5188436c3bc8027cc3be1257becdbfbfbf
stufisher <stu@q3f.org>
2014-12-02 11:43:44 +0000

    Add ajax fault handler

--- includes/pages/class.fault.ajax.php
+++ includes/pages/class.fault.ajax.php
@@ -5,7 +5,39 @@
     
     class Ajax extends AjaxBase {
         
-        var $arg_list = array('time' => '\d+', 'bl' => '\w\d\d(-\d)?', 'sid' => '\d+', 'cid' => '\d+', 'scid' => '\d+', 'pp' => '\d+', 'page' => '\d+', 'array' => '\d', 'ty' => '\w+', 'fid' => '\d+', 'name' => '[A-Za-z0-9_\- ]+', 'desc' => '[A-Za-z0-9_\- ]+', 's' => '\w+', 'id' => '\d+', 'term' => '\w+', 'visit' => '\w+\d+-\d+');
+        var $arg_list = array('time' => '\d+',
+                              'bl' => '\w\d\d(-\d)?',
+                              'sid' => '\d+',
+                              'cid' => '\d+',
+                              'scid' => '\d+',
+                              'pp' => '\d+',
+                              'page' => '\d+',
+                              'array' => '\d',
+                              'ty' => '\w+',
+                              'fid' => '\d+',
+                              'name' => '[A-Za-z0-9_\- ]+',
+                              'desc' => '[A-Za-z0-9_\- ]+',
+                              's' => '\w+', 'id' => '\d+',
+                              'term' => '\w+',
+                              'visit' => '\w+\d+-\d+',
+                              
+                              'beamline' => '\w\d\d(-\d)?',
+                              'start' => '\d\d-\d\d-\d\d\d\d \d\d:\d\d',
+                              'end' => '\d\d-\d\d-\d\d\d\d \d\d:\d\d',
+                              'blstart' => '\d\d-\d\d-\d\d\d\d \d\d:\d\d',
+                              'blend' => '\d\d-\d\d-\d\d\d\d \d\d:\d\d',
+                              'sub_component' => '\d+',
+                              'beamtime_lost' => '\d',
+                              'resolved' => '\d',
+                              'session' => '\d+',
+                              
+                              'title' => '.*',
+                              'desc' => '.*',
+                              'resolution' => '.*',
+                              
+                              'assignee' => '\w+\d+',
+                              );
+        
         var $dispatch = array('list' => '_get_faults',
                               
                               'visits' => '_get_visits',
@@ -492,6 +524,84 @@
                                   
         }
 
+                                  
+        # ------------------------------------------------------------------------
+        # Add fault via ajax
+        function _add_fault() {
+            $valid = True;
+            foreach (array('title', 'desc', 'session', 'start', 'beamtime_lost', 'resolved') as $f) {
+                if (!$this->has_arg($f)) $valid = False;
+            }
+            
+            if (!$valid) $this->_error('Missing Fields');
+            
+            $btlstart = $this->has_arg('blstart') ? $this->arg('blstart') : '';
+            $btlend = $this->has_arg('blend') ? $this->arg('blend') : '';
+            $end = $this->has_arg('end') ? $this->arg('end') : '';
+            $as = $this->has_arg('assignee') ? $this->arg('assignee') : '';
+            
+            $this->db->pq("INSERT INTO bf_fault (faultid, sessionid, owner, subcomponentid, starttime, endtime, beamtimelost, beamtimelost_starttime, beamtimelost_endtime, title, description, resolved, resolution, assignee) VALUES (s_bf_fault.nextval, :1, :2, :3, TO_DATE(:4, 'DD-MM-YYYY HH24:MI'), TO_DATE(:5, 'DD-MM-YYYY HH24:MI'), :6, TO_DATE(:7, 'DD-MM-YYYY HH24:MI'), TO_DATE(:8, 'DD-MM-YYYY HH24:MI'), :9, :10, :11, :12, :13) RETURNING faultid INTO :id", array($this->arg('session'), phpCAS::getUser(), $this->arg('sub_component'), $this->arg('start'), $end, $this->arg('beamtime_lost'), $btlstart, $btlend, $this->arg('title'), $this->arg('desc'), $this->arg('resolved'), $this->arg('resolution'), $as));
+                    
+            $newid = $this->db->id();
+
+            $info = $this->db->pq("SELECT p.proposalcode || p.proposalnumber || '-' || bl.visit_number as visit, s.name as system, c.name as component, sc.name as subcomponent, TO_CHAR(f.starttime, 'DD-MM-YYYY HH24:MI') as starttime, TO_CHAR(f.endtime, 'DD-MM-YYYY HH24:MI') as endtime, f.beamtimelost, round((f.beamtimelost_endtime-f.beamtimelost_starttime)*24,2) as lost, f.title, f.resolved, f.resolution, f.description, TO_CHAR(f.beamtimelost_endtime, 'DD-MM-YYYY HH24:MI') as beamtimelost_endtime, TO_CHAR(f.beamtimelost_starttime, 'DD-MM-YYYY HH24:MI') as beamtimelost_starttime, f.owner
+                FROM ispyb4a_db.bf_fault f
+                INNER JOIN bf_subcomponent sc ON f.subcomponentid = sc.subcomponentid
+                INNER JOIN bf_component c ON sc.componentid = c.componentid
+                INNER JOIN bf_system s ON c.systemid = s.systemid
+                INNER JOIN blsession bl ON f.sessionid = bl.sessionid
+                INNER JOIN proposal p ON bl.proposalid = p.proposalid
+
+                WHERE f.faultid=:1", array($newid));
+            
+            $info = $info[0];
+                                
+            foreach (array('DESCRIPTION', 'RESOLUTION') as $k) {
+                #if ($info[$k]) {
+                    #$info[$k] = Markdown::defaultTransform($info[$k]->read($info[$k]->size()));
+                $info[$k] = $this->db->read($info[$k]);
+                #}
+            }
+                                  
+            $report = '<b>'.$info['TITLE'].'</b><br/><br/>Reported By: '.$this->_get_name($info['OWNER']).'<br/><br/>System: '.$info['SYSTEM'].'<br/>Component: '.$info['COMPONENT'].' &raquo; '.$info['SUBCOMPONENT'].'<br/><br/>Start: '.$info['STARTTIME'].' End: '.($info['RESOLVED'] == 1 ? $info['ENDTIME'] : 'N/A') .'<br/>Resolved: '.($info['RESOLVED']  == 2 ? 'Partial' : ($info['RESOLVED'] ? 'Yes' : 'No')).'<br/>Beamtime Lost: '.($info['BEAMTIMELOST'] ? ('Yes ('.$info['LOST'].'h between '.$info['BEAMTIMELOST_STARTTIME'].' and '.$info['BEAMTIMELOST_ENDTIME'].')') : 'No').'<br/><br/><b>Description</b><br/>'.$info['DESCRIPTION'].'<br/><br/>'.($info['RESOLVED'] ? ('<b>Resolution</b><br/>'.$info['RESOLUTION']):'').'<br/><br/><a href="http://ispyb.diamond.ac.uk/fault/fid/'.$this->db->id().'">Fault Report Link</a>';
+                                  
+            $data = array('txtTITLE'      => 'Fault Report: '. $info['TITLE'],
+                          'txtCONTENT'    => $report,
+                          'txtLOGBOOKID'  =>'BL'.strtoupper
+($this->arg('beamline')),
+                          'txtGROUPID'    => 'GEN',
+                          'txtENTRYTYPEID'=> '41',
+                          'txtUSERID'     => phpCAS::getUser(),
+                          'txtMANUALAUTO' => 'M',
+                          );
+            
+            
+            if ($_FILES['userfile1']['name']) {
+                move_uploaded_file($_FILES['userfile1']['tmp_name'], '/tmp/fault_'.strtolower($_FILES['userfile1']['name']));
+                $data['userfile1'] = '@/tmp/fault_'.strtolower($_FILES['userfile1']['name']);
+            }
+            
+            $ch = curl_init('http://rdb.pri.diamond.ac.uk/php/elog/cs_logentryext_bl.php');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            $response = curl_exec($ch);
+            curl_close($ch);
+
+            
+            if (preg_match('/New Log Entry ID:(\d+)/', $response, $eid)) {
+                $this->db->pq('UPDATE ispyb4a_db.bf_fault SET elogid=:1 WHERE faultid=:2', array($eid[1], $newid));
+            }
+                                  
+            if (preg_match('/Attachment Id:(\d+)/', $response, $aid)) {
+                $this->db->pq('UPDATE ispyb4a_db.bf_fault SET attachment=:1 WHERE faultid=:2', array($aid[1].'-fault_'.strtolower($_FILES['userfile1']['name']), $newid));
+            }
+
+                                  
+            $this->_output(array('FAULTID' => $newid));
+        }
+                                  
     }
 
 ?>
